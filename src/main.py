aa6dd8d461c78e16fd9ead64f407cd0708135716
Nurzhan <nurzhan.abdikaparov@gmail.com>
2026-05-02 00:28:43 +0500

    feat: explore data types

--- src/main.py
+++ src/main.py
@@ -10,3 +10,17 @@ def add_numbers(a, b):
 
 
 print(add_numbers(5, 5))
+
+age = 25
+price = 3.14
+name = "Nurzhan"
+is_developer = False
+skills = ["Python", "Git", "VS Code"]
+person = {"name": "Nurzhan", "age": 25}
+
+print(type(age))
+print(type(price))
+print(type(name))
+print(type(is_developer))
+print(type(skills))
+print(type(person))
